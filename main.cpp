87524fc749a1f0ba6cc37296e710a3083866e216
Hane Liu <hane0131@gmail.com>
2015-12-29 16:27:22 +0800

    preset main code

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+//#include <vector>
+//#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    //game start
     cout << " -----------------------------" << endl
-         //<< "|                             |" << endl
          << "|     Plants v.s Zombies      |" << endl
-         //<< "|                             |" << endl
          << " -----------------------------" << endl;
     constexpr int LAND_DEFAULT=8;
     constexpr int LAND_MAX=10;
@@ -20,7 +21,7 @@ int main()
     constexpr int ZOMBIE_MIN=1;
 
     string input;
-
+    //initialize game setting
     cout << "Number of lands on the map (" << LAND_MIN << "-" << LAND_MAX << ", default: " << LAND_DEFAULT <<")...>";
     int value = LAND_DEFAULT;
     getline(cin, input);
@@ -59,6 +60,86 @@ int main()
          << "=============================================================================" << endl;
     system("pause");
     system("cls");
+
+    //game start
+    /*
+    //construct
+    Player *player = new Player();
+    Zombie *zombie = new Zombie()[ZOMBIES];
+    Land *land = new Land()[LANDS];
+    Map *map = new Map(land);
+    vector<Plant*> plant;
+    ifstream fin=open("plants.txt");
+    while(!fin.eof())
+    {
+        char type;
+        fin << type;
+        if(fin.eof()) break;
+        Plant *tmp = nullptr;
+        switch(type)
+        {
+        case 'C':
+            {
+                tmp = new CoinPlant(fin);
+            }
+        case 'S':
+            {
+                tmp = new HornPlant(fin);
+            }
+        case 'B':
+            {
+                tmp = new BombPlant(fin);
+            }
+        case 'H':
+            {
+                tmp = new HealPlant(fin);
+            }
+        default:
+            continue;
+        }
+        if(tmp)
+        {
+            plant.push_back(tmp);
+        }
+    }
+
+    cout << *map << endl;
+    cout << "------------------------------------------------" << endl;
+    cout << "Zombie information:" << endl;
+    for(int i=0;i<ZOMBIES;i++)
+    {
+        cout << '[' << i << "] " << zombie[i];
+    }
+    cout << endl << "================================================" << endl;
+    for(int i=0;i<plant.size();++i)
+    {
+        cout << '[' << i << "] " ;
+        switch(plant[i]->type())
+        {
+            case 'C':
+                cout << dynamic_cast<CoinPlant *>(plant[i]);
+            case 'S':
+                cout << dynamic_cast<HornPlant *>(plant[i]);
+            case 'B':
+                cout << dynamic_cast<BombPlant *>(plant[i]);
+            case 'H':
+                cout << dynamic_cast<HealPlant *>(plant[i]);
+        }
+        cout << endl;
+    }
+    cout << endl << *player;
+    int choice = plant.size();
+    cout << ":    Enter your choice (" << choice << " to give up, default: " << choice << ")...>";
+
+
+    // destruct
+    while(!plant.empty())
+        delete plant.pop_back();
+    delete player;
+    delete map;
+    delete [] zombie;
+    delete [] land;
+    */
 
     return 0;
 }
