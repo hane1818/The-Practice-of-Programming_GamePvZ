bedc779a6ece08e801a0bebd63367d05c04141bf
Hane Liu <hane0131@gmail.com>
2015-12-29 13:02:02 +0800

    add code for asking player to define number of lands and zombies

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
+    cout << "-------------------------------" << endl
+         //<< "|                             |" << endl
+         << "|     Plants v.s Zombies      |" << endl
+         //<< "|                             |" << endl
+         << "-------------------------------" << endl;
+    constexpr int LAND_DEFAULT=8;
+    constexpr int LAND_MAX=10;
+    constexpr int LAND_MIN=1;
+    constexpr int ZOMBIE_DEFAULT=3;
+    constexpr int ZOMBIE_MAX=10;
+    constexpr int ZOMBIE_MIN=1;
+
+    string input;
+
+    cout << "Number of lands on the map (" << LAND_MIN << "-" << LAND_MAX << ", default: " << LAND_DEFAULT <<")...>";
+    int value = LAND_DEFAULT;
+    getline(cin, input);
+    if(!input.empty())
+    {
+        istringstream stream( input );
+        stream >> value;
+        if(value > LAND_MAX) value = LAND_MAX;
+        if(value < LAND_MIN) value = LAND_MIN;
+    }
+    const int LANDS=value;
+
+    cout << "Number of zombies on the map (" << ZOMBIE_MIN << "-" << ZOMBIE_MAX << ", default: "<< ZOMBIE_DEFAULT <<")...>";
+    value=ZOMBIE_DEFAULT;
+    getline(cin, input);
+    if(!input.empty())
+    {
+        istringstream stream( input );
+        stream >> value;
+        if(value > ZOMBIE_MAX) value = ZOMBIE_MAX;
+        if(value < ZOMBIE_MIN) value = ZOMBIE_MIN;
+    }
+    const int ZOMBIES=value;
 
     return 0;
 }
