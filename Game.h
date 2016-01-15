529b58dc14ee63cf10dc5365220e60961400074b
Hane Liu <hane0131@gmail.com>
2016-01-15 18:02:23 +0800

    initial commit

---
+++ Game.h
@@ -0,0 +1,17 @@
+#ifndef GAME__
+#define GAME__
+
+class Game
+{
+public:
+    static constexpr int LAND_DEFAULT=8;
+    static constexpr int LAND_MAX=10;
+    static constexpr int LAND_MIN=1;
+    static constexpr int ZOMBIE_DEFAULT=3;
+    static constexpr int ZOMBIE_MAX=10;
+    static constexpr int ZOMBIE_MIN=1;
+private:
+
+};
+
+#endif // GAME__
