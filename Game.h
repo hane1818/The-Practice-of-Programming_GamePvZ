6d6fbbef3355157d36cc4f2a5aee70d40c61abd1
Hane Liu <hane0131@gmail.com>
2016-01-15 18:17:01 +0800

    add default constructor

--- Game.h
+++ Game.h
@@ -8,6 +8,7 @@
 class Game
 {
 public:
+    Game()=default;
     static constexpr int LAND_DEFAULT=8;
     static constexpr int LAND_MAX=10;
     static constexpr int LAND_MIN=1;
@@ -15,9 +16,9 @@ public:
     static constexpr int ZOMBIE_MAX=10;
     static constexpr int ZOMBIE_MIN=1;
 private:
-    Player *player_=nullptr;
-    Zombie *zombie_=nullptr;
-    Map *map_=nullptr;
+    Player *player_=new Player;
+    Zombie *zombie_=new Zombie[ZOMBIE_DEFAULT];
+    Map *map_=new Map(LAND_DEFAULT);
     vector<Plant*> plant_;
     int numOfZombie_=ZOMBIE_DEFAULT;
     int numOfLand_=LAND_DEFAULT;
